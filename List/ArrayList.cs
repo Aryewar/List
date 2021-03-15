dc572433965bb55c95e626dbcdf13bc541ac21ce
Maxim <aryewar@gmail.com>
2021-03-15 19:43:10 +0200

    Add methods RemoveLast, DownSize

--- List/ArrayList.cs
+++ List/ArrayList.cs
@@ -15,6 +15,14 @@ namespace List
             _array = new int[10];
         }
 
+        public ArrayList(int length)
+        {
+            Length = length;
+            length = (int)(length * 1.33 + 1);
+
+            _array = new int[length];
+        }
+
         public void Add(int value)
         {
             if(Length == _array.Length)
@@ -26,12 +34,38 @@ namespace List
             ++Length;
         }
 
+        public void RemoveLast()
+        {
+            if (Length != 0)
+            {
+                if ((Length == _array.Length / 2) && (Length > 10))
+                {
+                    DownSize();
+                }
+
+                --Length;
+            }
+        }
+
         private void UpSize()
         {
             int newLength = (int)(_array.Length * 1.33 + 1);
             int[] tmpArray = new int[newLength];
 
             for(int i = 0; i < _array.Length; ++i)
+            {
+                tmpArray[i] = _array[i];
+            }
+
+            _array = tmpArray;
+        }
+
+        private void DownSize()
+        {
+            int newLength = (int)(_array.Length * 0.33 + 1);
+            int[] tmpArray = new int[newLength];
+
+            for (int i = 0; i < _array.Length; ++i)
             {
                 tmpArray[i] = _array[i];
             }
