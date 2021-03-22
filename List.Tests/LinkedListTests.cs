e3ac38d8b2b53896b7111246f92d69bee056a061
Maxim <aryewar@gmail.com>
2021-03-22 14:09:14 +0200

    Add LinkedList, MyNode, LinkedListTests

---
+++ List.Tests/LinkedListTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace List.Tests
+{
+    class LinkedListTests
+    {
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3, 4 })]
+        [TestCase(new int[] { }, new int[] { 4 })]
+        public void Add_WhenAddOneNumber_ShouldAddOneNumberToEnd(int[] initialValue, int[] expectedValue)
+        {
+            ArrayList actual = new LinkedList(initialValue);
+            ArrayList expected = new LinkedList(expectedValue);
+
+            actual.Add(4);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ List/LinkedList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace List
+{
+    class ArrayList
+    {
+        public int Length { get; private set; }
+
+        private MyNode _head;
+        private MyNode _tail;
+
+
+        public ArrayList()
+        {
+            Length = 0;
+            _head = null;
+            _tail = null;
+        }
+
+        public ArrayList(int value)
+        {
+            Length = 1;
+            _head = new MyNode(value);
+            _tail = _head;
+        }
+
+        public ArrayList(int[] values)
+        {
+            Length = 0;
+
+            if (values.Length != 0)
+            {
+                _head = new MyNode(values[0]);
+                _tail = _head;
+                ++Length;
+
+                for (int i = 1; i < values.Length; ++i)
+                {
+                    Add(values[i]);
+                }
+            }
+            else
+            {
+                _head = null;
+                _tail = null;
+            }
+
+
+        }
+
+        public void Add(int value)
+        {
+            _tail.Next = new MyNode(value);
+            _tail = _tail.Next;
+            ++Length;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                MyNode currentNode = GetNodeByIndex(index);
+
+                return currentNode.Value;
+            }
+
+            set
+            {
+                MyNode currentNode = GetNodeByIndex(index);
+
+                currentNode.Value = value;
+            }
+        }
+
+        private MyNode GetNodeByIndex(int index)
+        {
+            MyNode currentNode = _head;
+
+            for(int i = 1; i <= index; ++i)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            return currentNode;
+        }
+    }
+}
